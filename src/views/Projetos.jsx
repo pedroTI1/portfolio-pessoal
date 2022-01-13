6f98dece0e1eb7bcd249f36f02f8c140cef307ef
Pedro Antonio <pedroantonioalvesdeoliveira1@gmail.com>
2022-01-12 22:19:04 -0300

    pequena adição

--- src/views/Projetos.jsx
+++ src/views/Projetos.jsx
@@ -10,6 +10,7 @@ import Calc from '../assets/calc.png'
 
 import Page from '../assets/page.png'
 
+import Java from '../assets/java.png'
 
 
 
@@ -24,9 +25,10 @@ const Projetos = props => (
             description="Landing Page de uma Agência. Projeto desenvolvido durante o curso de front-end da DankiCode. O objetivo era treinar os conhecimentos em HTML e CSS."
             image={Page}
             link="https://github.com/pedroTI1/paginaHTML" />
-        <Projeto title="Calculadora"
-            description="Landing Page de uma Agência. Projeto desenvolvido durante o curso de front-end da DankiCode. O objetivo era treinar os conhecimentos em HTML e CSS."
-            image={Calc} />
+        <Projeto title="Bank Account"
+            description="Parte back-end de um trabalho final da faculdade. O projeto consistia em realizar empréstimos para clientes e foi desenvolvido utilizando tecnologias como Spring Boot, JPA/Hibernate, Thymeleaf."
+            image={Java} 
+            link = "https://github.com/pedroTI1/emprestimo"/>
         <div className="btn-portfolio">
             <a href="https://github.com/pedroTI1" target="_blank" rel="noreferrer">Confira mais projetos aqui!</a>
         </div>
