01a2a43ed53f5092d7be62859ec7c3ab000cc6ae
Pedro Antonio <pedroantonioalvesdeoliveira1@gmail.com>
2022-01-14 04:59:11 -0300

    pequenos ajustes

--- src/views/Header.jsx
+++ src/views/Header.jsx
@@ -7,25 +7,30 @@ import './Header.css'
 import Curriculo from '../assets/cv__PedroAntonio.pdf'
 
 
-const Header = props => (
-    <main id="header" className="Header">
-        <nav>
-            <h2>Pedro Antônio</h2>
-            <ul>
-                <li><AnchorLink href='#'>Inicio</AnchorLink></li>
-                <li><AnchorLink href='#projetos'>Projetos</AnchorLink></li>
-                <li><AnchorLink href='#contato'>Contato</AnchorLink></li>
-            </ul>
-        </nav>
-        <div className="center">
-            <p>Olá! Meu nome é <span>Pedro Antônio</span></p>
-            <h1>Estudante de Engenharia da Computação e Desenvolvedor Web</h1>
-            <div className="btn">
-                <a href={Curriculo}>Baixar Curriculo</a>
+const Header = (props) => {
+    return (
+        <main id="header" className="Header">
+            <div className="header-nav">
+                <h2>Pedro Antônio</h2>
+                <nav className="navbar">
+                    <ul>
+                        <li><AnchorLink href='#'>Inicio</AnchorLink></li>
+                        <li><AnchorLink href='#projetos'>Projetos</AnchorLink></li>
+                        <li><AnchorLink href='#contato'>Contato</AnchorLink></li>
+                    </ul>
+                </nav>
             </div>
-        </div>
-    </main>
-)
+            <div className="center">
+                <p>Olá! Meu nome é <span>Pedro Antônio</span></p>
+                <h1>Estudante de Engenharia da Computação e Desenvolvedor Web</h1>
+                <div className="btn">
+                    <a href={Curriculo}>Baixar Curriculo</a>
+                </div>
+            </div>
+        </main>
+    )
+
+}
 
 
 export default Header
