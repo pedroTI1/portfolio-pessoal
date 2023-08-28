f86d64d72b5a54b13d86e0a79fc08e02ba17176b
Pedro Antonio <pedroantonioalvesdeoliveira1@gmail.com>
2023-08-28 15:17:37 -0300

    update cv and github link

--- src/views/Projetos.jsx
+++ src/views/Projetos.jsx
@@ -20,17 +20,17 @@ const Projetos = props => (
         <Projeto title="Calculadora"
             description="Projeto de uma calculadora simples com um seletor de temas. O projeto foi feito utilizando HTML, CSS e JavaScript Puro."
             image={Calc}
-            link="https://github.com/pedroTI1/projetoCalculadora" />
+            link="https://github.com/ppedroalves/projetoCalculadora" />
         <Projeto title="LP Agência Bold"
             description="Landing Page de uma Agência. Projeto desenvolvido durante o curso de front-end da DankiCode. O objetivo era treinar os conhecimentos em HTML e CSS."
             image={Page}
-            link="https://github.com/pedroTI1/paginaHTML" />
+            link="https://github.com/ppedroalves/paginaHTML" />
         <Projeto title="Bank Account"
             description="Parte back-end de um trabalho final da faculdade. O projeto consistia em realizar empréstimos para clientes e foi desenvolvido utilizando tecnologias como Spring Boot, JPA/Hibernate, Thymeleaf."
             image={Java} 
-            link = "https://github.com/pedroTI1/emprestimo"/>
+            link = "https://github.com/ppedroalves/emprestimo"/>
         <div className="btn-portfolio">
-            <a href="https://github.com/pedroTI1" target="_blank" rel="noreferrer">Confira mais projetos aqui!</a>
+            <a href="https://github.com/ppedroalves" target="_blank" rel="noreferrer">Confira mais projetos aqui!</a>
         </div>
 
     </main>
